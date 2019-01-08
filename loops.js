dab846c90c1a71f8997012022057697dadc1d9a9
Brittany Malooly <bmalooly@gmail.com>
2019-01-08 00:39:27 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -8,7 +8,7 @@ function forLoop(array) {
   return array;
 }
 function whileLoop(num) {
-  while (num > 0) {console.log(--countdown);
+  while (num > 0) {console.log(--num);
   }
   return 'done'
 }
